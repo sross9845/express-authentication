f137348bcd73f7bda8f6fa3313ebbf70148c37ea
sross9845 <sean.ross.985@gmail.com>
2019-10-17 09:24:51 -0700

    initial commit got log in and sign up done

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
 
 router.get('/signup', function(req, res) {
+
   res.render('auth/signup');
+});
+
+router.post('/signup', function(req, res) {
+  //find or create user
+  db.user.findOrCreate({
+    where:{
+      email: req.body.email
+    },
+    defaults:{
+      name: req.body.name,
+      password: req.body.password
+    }
+  }).then(function([user, created]){
+    if(created){
+      //else sign user up through form and redirect to home
+      console.log('User successfully created')
+      res.redirect('/')
+    } else {
+      //if user existed error and redirect to sign up
+      console.log('Email already exists')
+      res.redirect('/auth/signup')
+    }
+  }).catch(function(err){
+    //catch errors
+    console.log(err);
+    res.redirect('/auth/signup');
+  })
 });
 
 router.get('/login', function(req, res) {
--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
+const session = require('express-session')
 const app = express();
+const passport = require('./config/ppConfig')
 
 app.set('view engine', 'ejs');
-
 app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname + "/public"));
+app.use(express.static('public'));
 app.use(ejsLayouts);
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+//come after session setup
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', function(req, res) {
   res.render('index');
@@ -23,3 +33,4 @@ app.use('/auth', require('./controllers/auth'));
 var server = app.listen(process.env.PORT || 3000);
 
 module.exports = server;
+
